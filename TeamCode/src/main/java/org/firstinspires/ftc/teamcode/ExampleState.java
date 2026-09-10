55a14c99a70b4cf4e17d3e9803983649424ccc77
queso <thepstricklin@gmail.com>
2026-09-09 19:59:13 -0700

    State machine ig

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleState.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+public class ExampleState extends RobotState {
+    public ExampleState() {}
+
+    @Override
+    public void onStateStart() {
+    }
+
+    @Override
+    public void onStateLoop() {
+    }
+
+    @Override
+    public void onStateEnd() {
+
+    }
+}
