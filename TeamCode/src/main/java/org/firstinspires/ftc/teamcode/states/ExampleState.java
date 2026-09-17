07347669918ea0a8bef3acce3875b753cd61717b
abelgique <alejandrobelgique@gmail.com>
2026-09-17 15:08:14 -0700

    Fixed Errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/ExampleState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/ExampleState.java
@@ -1,12 +1,9 @@
 package org.firstinspires.ftc.teamcode.states;
-<<<<<<< Updated upstream
-=======
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard;
 
->>>>>>> Stashed changes
 public class ExampleState extends RobotState {
 
     public ExampleState(ProgrammingBoard board, Gamepad gamepad1, Gamepad gamepad2) {
@@ -19,13 +16,6 @@ public class ExampleState extends RobotState {
 
     @Override
     public void onStateLoop() {
-<<<<<<< Updated upstream
-        if (stateTimer.seconds() > 10) {
-            transitionState(new ExampleState2());
-        }
-=======
-
->>>>>>> Stashed changes
     }
 
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/ExampleState2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/ExampleState2.java
@@ -1,7 +1,13 @@
 package org.firstinspires.ftc.teamcode.states;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard;
+
 public class ExampleState2 extends RobotState {
-    public ExampleState2() {}
+    public ExampleState2(ProgrammingBoard board, Gamepad gamepad1, Gamepad gamepad2) {
+        super(board, gamepad1, gamepad2);
+    }
 
     @Override
     public void onStateStart() {
@@ -9,9 +15,6 @@ public class ExampleState2 extends RobotState {
 
     @Override
     public void onStateLoop() {
-        if (stateTimer.seconds() > 10) {
-            transitionState(new ExampleState());
-        }
     }
 
     @Override
