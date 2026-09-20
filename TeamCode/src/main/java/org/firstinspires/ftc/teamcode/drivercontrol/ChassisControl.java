9fe0dc245ed9fa5f0765aca0459124a08907ccbb
QuesoYFrijoles <thepstricklin@gmail.com>
2026-09-20 09:59:20 -0700

    Created slowMode
    Created a slowmode that toggles on the rising edge of an a button press.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivercontrol/ChassisControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivercontrol/ChassisControl.java
@@ -3,21 +3,34 @@ package org.firstinspires.ftc.teamcode.drivercontrol;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;import org.firstinspires.ftc.teamcode.mechanisms.BaseChassisBoard;
-import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard;
 
 
 public class ChassisControl extends DriverControl<BaseChassisBoard> {
+    private boolean slowMode;
+    private double slowModeMult;
+
     @Override
     public void init(Telemetry telemetry, BaseChassisBoard board, Gamepad gamepad1, Gamepad gamepad2) {
         this.board = board;
         this.gamepad1 = gamepad1;
         this.gamepad2 = gamepad2;
         this.telemetry = telemetry;
+        slowMode = false;
+        slowModeMult = 0.33;
     }
     @Override
     public void loop() {
         currentVoltage = board.getCachedVoltage();
         final double DEADZONE = .05;
+
+        if (gamepad1.aWasPressed()) {
+            slowMode = !slowMode;
+        }
+
+        handleDrivetrain(DEADZONE);
+    }
+    private void handleDrivetrain(final double DEADZONE) {
+        currentVoltage = board.getCachedVoltage();
         double axial = -quadraticScale(applyDeadzone(gamepad1.left_stick_y, DEADZONE));
         double lateral = quadraticScale(applyDeadzone(gamepad1.left_stick_x, DEADZONE));
         double yaw = quadraticScale(applyDeadzone(gamepad1.right_stick_x, DEADZONE));
@@ -39,14 +52,19 @@ public class ChassisControl extends DriverControl<BaseChassisBoard> {
         blPower = voltageClamp(blPower);
         brPower = voltageClamp(brPower);
 
+        if (slowMode) {
+            flPower *= slowModeMult;
+            frPower *= slowModeMult;
+            blPower *= slowModeMult;
+            brPower *= slowModeMult;
+        }
+
         board.setFlMotorPower(flPower);
         board.setFrMotorPower(frPower);
         board.setBlMotorPower(blPower);
         board.setBrMotorPower(brPower);
 
-        // Show the elapsed game time and wheel power.
         telemetry.addData("Front left/Right", "%4.2f, %4.2f", flPower, frPower);
         telemetry.addData("Back  left/Right", "%4.2f, %4.2f", blPower, brPower);
-        telemetry.update();
     }
 }
